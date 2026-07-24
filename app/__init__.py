d5e178f206871366dcdda76e5029031cf120eb6d
Mesas <asesorautorizado.duilio@gmail.com>
2026-07-24 00:25:41 -0500

    Update __init__.py
    AAA

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,1 @@
-# En la raíz del proyecto:
-touch app/__init__.py
-touch app/core/__init__.py
-touch app/models/__init__.py
-touch app/routes/__init__.py
-touch app/schemas/__init__.py
+"""MITA - Servicios Técnicos a Domicilio"""
